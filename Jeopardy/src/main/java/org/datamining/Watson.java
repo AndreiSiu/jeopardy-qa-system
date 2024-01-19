eea14702809f71bf73e7d8802dca5dd625897dcc
AndreiSiu <vasiu_andrei2000@yahoo.com>
2024-01-19 14:55:56 +0200

    Updating project structure

--- Jeopardy/src/main/java/org/datamining/Watson.java
+++ Jeopardy/src/main/java/org/datamining/Watson.java
@@ -22,126 +22,99 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * The Watson class simulates the IBM Watson's Jeopardy problem by processing
- * and answering questions from a provided file, and evaluating the answers.
- */
 public class Watson {
-    private final List<String[]> correctAnswers;
-    private double answeredRight = 0.0;
-    private double sumReciprocalRank = 0.0;
-    private double answeredTotal = 0.0;
-    private StandardAnalyzer analyzer;
-    private FSDirectory index;
 
-    /**
-     * Constructs a Watson instance for answering questions and evaluating performance.
-     *
-     * @param indexDirectory Path to the directory containing the indexed data.
-     * @param questionFilePath Path to the file containing the questions.
-     */
-    public Watson(String indexDirectory, String questionFilePath) throws IOException {
-        this.index = FSDirectory.open(Paths.get(indexDirectory));
-        this.analyzer = new StandardAnalyzer();
-        this.correctAnswers = new ArrayList<>();
+    private final List<String[]> correctAnswers;
+    double answeredRight = 0.0;
+    double sumReciprocalRank = 0.0;
+    double answeredTotal = 0.0;
+    String inputDirectory = "";
+    String questionFile = "";
+    StandardAnalyzer analyzer;
+    IndexWriterConfig config;
+    IndexWriter writer;
+    FSDirectory index;
+
+    public Watson(String directoryName, String questionFilePath) throws IOException {
+        inputDirectory = directoryName;
+        questionFile = questionFilePath;
+        analyzer = new StandardAnalyzer();
+        config = new IndexWriterConfig(analyzer);
+        config.setOpenMode(OpenMode.CREATE_OR_APPEND);
+        index = FSDirectory.open(Paths.get("Jeopardy/src/main/resources/index"));
+        correctAnswers = new ArrayList<>();
     }
 
-    /**
-     * The main entry point of the Watson class.
-     *
-     */
     public static void main(String[] args) {
         try {
-            Watson watsonEngine = new Watson("Jeopardy/src/main/resources/index", "Jeopardy/src/main/resources/questions.txt");
-            watsonEngine.parseQuestions();
-            watsonEngine.printScore();
+            String directory = "../DataSets/wikipediaPages";
+            String questionFile = "../DataSets/questions.txt";
+            Watson jEngine = new Watson(directory, questionFile);
+
+            jEngine.parseQuestions(questionFile);
+            jEngine.printScore();
             System.out.println("Program completed, Thank you.");
         } catch (Exception ex) {
-            System.out.println("Error occurred: " + ex.getMessage());
+            System.out.println(ex.getMessage());
         }
     }
 
-    /**
-     * Processes and answers the questions from the file, updating performance metrics.
-     */
-    public void parseQuestions() throws IOException, ParseException {
-        try (Scanner scanner = new Scanner(new File("Jeopardy/src/main/resources/questions.txt"))) {
-            while (scanner.hasNextLine()) {
-                String category = scanner.nextLine();
-                String query = scanner.nextLine() + " " + category.replaceAll("\\r\\n", "");
-                String answer = scanner.nextLine();
-                scanner.nextLine(); // Skip the blank line
-
-                answerQuestions(query, answer.split("\\|"));
-            }
-        }
-    }
-
-    /**
-     * Answers the provided question using the indexed data, updating performance metrics.
-     *
-     * @param query The query representing the question.
-     * @param expectedAnswers The expected answers for the question.
-     */
-    private void answerQuestions(String query, String[] expectedAnswers) throws IOException, ParseException {
-        QueryParser parser = new QueryParser("tokens", analyzer);
-        IndexReader reader = DirectoryReader.open(index);
-        IndexSearcher searcher = new IndexSearcher(reader);
-        Query luceneQuery = parser.parse(QueryParser.escape(query));
-        TopDocs docs = searcher.search(luceneQuery, 1);
-
-        for (ScoreDoc scoreDoc : docs.scoreDocs) {
-            Document retrievedDoc = searcher.doc(scoreDoc.doc);
-            evaluateAnswer(retrievedDoc.get("title").trim(), expectedAnswers);
-        }
-    }
-
-    /**
-     * Evaluates the retrieved answer against the expected answers, updating performance metrics.
-     *
-     * @param retrievedAnswer The answer retrieved by the system.
-     * @param expectedAnswers The expected answers for the question.
-     */
-    private void evaluateAnswer(String retrievedAnswer, String[] expectedAnswers) {
+    public void parseQuestions(String questionFile) throws java.io.FileNotFoundException, java.io.IOException, ParseException {
+        Scanner scanner = new Scanner(new File(questionFile));
         int currentRank = 1;
-        for (String expectedAnswer : expectedAnswers) {
-            if (expectedAnswer.trim().equals(retrievedAnswer)) {
-                answeredRight++;
-                sumReciprocalRank += 1.0 / currentRank;
-                correctAnswers.add(new String[]{retrievedAnswer, expectedAnswer});
-                break;
+        while (scanner.hasNextLine()) {
+            String category = scanner.nextLine();
+            String query = scanner.nextLine();
+            String answer = scanner.nextLine();
+            scanner.nextLine();
+            String[] answerArr = answer.split("\\|");
+            query = query + " " + category;
+            query = query.replaceAll("\\r\\n", "");
+            Query q = new QueryParser("tokens", analyzer).parse(QueryParser.escape(query));
+            IndexReader reader = DirectoryReader.open(index);
+            IndexSearcher searcher = new IndexSearcher(reader);
+            TopDocs docs = searcher.search(q, 1);
+            ScoreDoc[] hits = docs.scoreDocs;
+            for (ScoreDoc s : hits) {
+                Document answerDoc = searcher.doc(s.doc);
+                for (String ans : answerArr) {
+                    ans = ans.trim();
+                    if (ans.equals(answerDoc.get("title"))) {
+                        answeredRight++;
+                        sumReciprocalRank += 1.0 / currentRank;
+                        correctAnswers.add(new String[]{query, ans, answerDoc.get("title")});
+                        break;
+                    }
+                }
+                currentRank++;
             }
-            currentRank++;
+            answeredTotal++;
         }
-        answeredTotal++;
+        scanner.close();
     }
 
-    /**
-     * Prints the precision and mean reciprocal rank (MRR) scores, along with the error analysis.
-     */
     void printScore() {
         double precision = answeredRight / answeredTotal;
-        double mrr = sumReciprocalRank / answeredTotal;
-
         System.out.println("\tPrecision: " + precision);
+
+        double mrr = sumReciprocalRank / answeredTotal;
         System.out.println("\tMean Reciprocal Rank (MRR): " + mrr);
+
         System.out.println("\tTotal Questions Processed: " + answeredTotal);
         printErrorAnalysis();
     }
 
-    /**
-     * Prints the error analysis, detailing the number of correct and incorrect answers.
-     */
     void printErrorAnalysis() {
         System.out.println("\nError Analysis:");
         System.out.println("Number of Correct Answers: " + correctAnswers.size());
-        System.out.println("Number of Incorrect Answers: " + (answeredTotal - correctAnswers.size()));
+        System.out.println("Number of Incorrect Answers: " + (int)(answeredTotal-correctAnswers.size()));
 
-        for (String[] correctAnswerInfo : correctAnswers) {
-            System.out.println("Question: " + correctAnswerInfo[0]);
-            System.out.println("Expected Answer: " + correctAnswerInfo[1]);
-            System.out.println("Correct Answer Provided: " + correctAnswerInfo[0]);
+        for (String[] error : correctAnswers) {
+            System.out.println("Question: " + error[0]);
+            System.out.println("Expected Answer: " + error[1]);
+            System.out.println("Correct Answer Provided: " + error[2]);
             System.out.println("------");
         }
     }
+
 }
